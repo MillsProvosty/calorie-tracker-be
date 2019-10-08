79026513e3446e886f0ea6f59a3e318f25411937
Jake Miller <jake0miller@gmail.com>
2019-10-08 15:20:43 -0600

    Update app.js to use nested foods directory

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/api/v1/foods');
+var foodRouter = require('./routes/api/v1/foods/foods');
 
 var app = express();
 
@@ -16,6 +16,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/v1/foods', usersRouter);
+app.use('/api/v1/foods', foodRouter);
 
 module.exports = app;
