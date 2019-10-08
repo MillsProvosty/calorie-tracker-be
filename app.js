d1e4cf767a52e237daa65415b34c257b7452ee66
Jake Miller <jake0miller@gmail.com>
2019-10-08 15:07:26 -0600

    Add route to /api/v1/foods

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/api/v1/foods');
 
 var app = express();
 
@@ -16,6 +16,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/foods', usersRouter);
 
 module.exports = app;
