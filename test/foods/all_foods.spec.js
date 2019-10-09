5e8a71f3b85f22d3fb61e49600fc85bd4b761e30
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-10-08 18:47:43 -0600

    Deletes --env test on food by id and all foods tests

--- test/foods/all_foods.spec.js
+++ test/foods/all_foods.spec.js
@@ -4,11 +4,11 @@ const app = require('../../app')
 
 describe('api', () => {
   beforeAll(() => {
-    shell.exec('npx sequelize db:migrate --env test')
-    shell.exec('npx sequelize db:seed:all --env test')
+    shell.exec('npx sequelize db:migrate')
+    shell.exec('npx sequelize db:seed:all')
   })
   afterAll(() => {
-    shell.exec('npx sequelize db:migrate:undo:all --env test')
+    shell.exec('npx sequelize db:migrate:undo:all')
   })
 
   describe('Test GET /api/v1/foods path', () => {
