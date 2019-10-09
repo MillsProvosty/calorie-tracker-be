07c9707d2d58f2c42297a541d3aa33b5fc4045d8
Jake Miller <jake0miller@gmail.com>
2019-10-08 19:37:03 -0600

    Add after all command to ensure no open handles

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,6 +3,11 @@ const request = require("supertest")
 const app = require('../app')
 
 describe('api', () => {
+  afterAll(async () => {
+    shell.exec('npx sequelize db:migrate:undo:all')
+    await new Promise(resolve => setTimeout(() => resolve(), 500))
+  })
+  
   describe('Test GET /', () => {
 
     test('should reach index path', async () => {
