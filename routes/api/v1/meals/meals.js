e8559b3c8853e53e36dae75ec2b906c47734623f
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-10-10 13:52:59 -0600

    Create route for returning meal and associated meals in controller

--- routes/api/v1/meals/meals.js
+++ routes/api/v1/meals/meals.js
@@ -11,5 +11,13 @@ router.get('/', function(req, res, next) {
   .catch( error => res.status(204).send({error}) )
 })
 
+/*Returns all food for given meal */
+router.get('/:id/foods', function(req, res, next){
+  res.setHeader("Content-Type", "application/json")
+  Meal.findOne( { where: { id: req.params.id }, attributes: ['id', 'name', 'Food', 'id', 'name', 'calories'] })
+  .then( food => (food) ? res.status(200).send(meal) : res.status(404).send() )
+  .catch( error => res.status(500).send({error}) )
+})
+
 
 module.exports = router;
