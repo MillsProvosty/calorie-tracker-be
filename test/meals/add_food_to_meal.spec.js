99a84d4db274d3f0028a4cc2c9861a2a4dc449d3
Jake Miller <jake0miller@gmail.com>
2019-10-10 18:23:29 -0600

    Write sad path tests for adding food to meals

--- test/meals/add_food_to_meal.spec.js
+++ test/meals/add_food_to_meal.spec.js
@@ -12,15 +12,34 @@ describe('api', () => {
     await Food.destroy({where:{}})
   })
 
-  describe('Test POST /api/v1/meals/:id/foods path', () => {
-    test('should return 1 meal and its foods', async () => {
-      var service = { "food": { "name": "Cherry", "calories": "25"} };
+  describe('Test POST /api/v1/meals/:mealId/foods/:id path', () => {
+    test('should add the food to the meal', async () => {
+      let banana = await Food.create({name: 'Banana', calories: 150})
       let breakfast = await Meal.create({name: 'Breakfast'})
 
-      return request(app).post(`/api/v1/meals/${breakfast.id}/foods`).send()
+      return request(app).post(`/api/v1/meals/${breakfast.id}/foods/${banana.id}`).send()
       .then(response => {
         expect(response.status).toBe(201)
-        expect(response.body.message).toBe("Successfully added Cherry to Breakfast")
+        expect(response.body).toBe("Successfully added Banana to Breakfast")
+      })
+    })
+
+    test('should not work if the food is already in the meal', async () => {
+      let cherry = await Food.create({name: 'Cherry', calories: 150})
+      let breakfast = await Meal.create({name: 'Breakfast'})
+      await FoodMeal.create({FoodId: cherry.id, MealId: breakfast.id})
+
+      return request(app).post(`/api/v1/meals/${breakfast.id}/foods/${cherry.id}`).send()
+      .then(response => {
+        expect(response.status).toBe(400)
+        expect(response.body).toBe("Food already in meal.")
+      })
+    })
+
+    test('404 if either does not exist', async () => {
+      return request(app).post(`/api/v1/meals/0/foods/0`).send()
+      .then(response => {
+        expect(response.status).toBe(404)
       })
     })
   })
