8ea217c1ce9bd059d571e6ee9519aad6a0fcbfd8
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-10-13 14:56:44 -0600

    Install cors to app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+app.use(cors())
+
 
 var indexRouter = require('./routes/index');
 var foodRouter = require('./routes/api/v1/foods/foods');
