c4aae7493babb2a60b85ce8cc2d746b3e365d689
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-10-13 16:39:49 -0600

    Add coor const and app.use to app.js file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var foodRouter = require('./routes/api/v1/foods/foods');
@@ -15,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/api/v1/foods', foodRouter);
 app.use('/api/v1/meals', mealsRouter);
